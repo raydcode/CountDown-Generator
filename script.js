51c655f72650d7cff3b14aa07e9d2bd39e2d627c
Ray <qc@pop-os.localdomain>
2021-12-11 13:06:36 +0530

    dynamic date countdown

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-const countDownDate = '1 Jan 2022';
+
 const countDownElement = document.getElementById('countDown');
+const dateElement = document.getElementById('date');
+const reset = document.getElementById('reset');
 
-const countDownTimer = setInterval(() => {
-  const targetDate = new Date(countDownDate);
-  const currentDate = new Date();
-  const totalSeconds = (targetDate - currentDate) / 1000;
+let countDownDate = ''
+const startTimer = (countDownDate)=>{
+         
+   if(countDownDate !== ''){
+    setInterval(() => {
+      const targetDate = new Date(countDownDate);
+      const currentDate = new Date();
+      const totalSeconds = (targetDate - currentDate) / 1000;
+    
+      const days = Math.floor(totalSeconds / 3600 / 24);
+    
+      const hours = Math.floor((totalSeconds / 3600) % 24);
+      const minutes = Math.floor((totalSeconds / 60) % 60);
+      const seconds = Math.floor(totalSeconds) % 60;
+    
+      countDownElement.textContent = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} s`;
+    }, 1000);
+   }else{
+    countDownElement.textContent = `Please Enter target Date`;
+   
+   }
 
-  const days = Math.floor(totalSeconds / 3600 / 24);
+  
+}
 
-  const hours = Math.floor((totalSeconds / 3600) % 24);
-  const minutes = Math.floor((totalSeconds / 60) % 60);
-  const seconds = Math.floor(totalSeconds) % 60;
 
-  countDownElement.textContent = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} s`;
-}, 1000);
+dateElement.addEventListener('input',(e)=>{
+  countDownDate = dateElement.value;
+  startTimer(countDownDate);
+  
+});
 
+
+reset.addEventListener('click',()=>{
+  countDownDate = '';
+  window.location.reload();
+  countDownElement.textContent = `Please Enter target Date`;
+})
+
+
+countDownElement.textContent = `Please Enter target Date`;
 
